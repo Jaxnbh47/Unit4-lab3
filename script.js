29b2f7bc90e68f4a149955c9b39bb938d6bcc9f1
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-11-06 18:45:13 -0500

    master

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+
 document.querySelector("form").addEventListener("submit",   function (event){
     event.preventDefault(); 
 
     let data = new FormData(document.querySelector("form"))
-    let n = data.get("howmany")
-    let coins = data.get("cointype")
+    let n = data.get("howmany")//by name 
+    let coins = data.get("cointype") // by name
     for (let i = 0; i < n; i++)  {
-        document.querySelector("section").innerHTML += `<div class = "coinmaker ${coins}"></div>`;
+        document.querySelector("section") += `<div class = "coinmaker ${coins}"></div>`;
     }
+
 })
-
-
-const box = document.querySelector("div");
-
-box.addEventListener('click', function handleClick(event) {
-  box.remove();
-});
+document.querySelector("#deletecoin").addEventListener('click', function coins(event) {
+        event.target.remove();
+      });
