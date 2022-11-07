fe544a9edcb5770d675640cbacbe4a0722202644
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-11-07 18:10:23 -0500

    master

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ document.querySelector("form").addEventListener("submit",   function (event){
 
     let data = new FormData(document.querySelector("form"))
     let n = data.get("howmany")//by name 
-    let coins = data.get("cointype") // by name
+    let coins = data.get("cointype") //by name
     for (let i = 0; i < n; i++)  {
         document.querySelector("container").innerHTML += `<div class = "coinplus ${coins}"></div>`;
     }
-
-
 })
 
-document.querySelector(".deletecoin").addEventListener('click', function coins(event) {
+document.querySelector("#deletecoin").addEventListener('click', function coins(event) {
         event.target.remove();
       });
